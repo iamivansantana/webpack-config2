f60b084be67c78e29e0ce37377f9ee49ad767748
ivan santana <iam.ivansantana@gmail.com>
2021-12-24 18:12:54 -0600

    webpack config whit jsx - Done

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -1,6 +1,8 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common');
 const path = require('path');
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
+const { HotModuleReplacementPlugin } = require('webpack');
 
 /** @type {import('webpack').Configuration} */
 const devConfig = {
@@ -12,6 +14,17 @@ const devConfig = {
 			directory: path.join(__dirname, 'public'),
 		},
 		open: true,
+		hot: true,
+	},
+	plugins: [new HotModuleReplacementPlugin(), new ReactRefreshWebpackPlugin()],
+	devtool: 'eval-source-map',
+	module: {
+		rules: [
+			{
+				use: ['style-loader', 'css-loader', 'sass-loader'],
+				test: /.(css|sass|scss)$/,
+			},
+		],
 	},
 };
 
--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,9 @@ import logo from './icon-ghost.svg';
 const App = () => {
 	return (
 		<div className='app'>
-			<h1>Este es el primer cambio!!!</h1>
+			<h1>Este es el segundo cambio!!!</h1>
 			<img src={logo} width='50' alt='ghost logo' />
+			<input type='text' />
 		</div>
 	);
 };
